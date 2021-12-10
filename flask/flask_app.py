2b64beb8ce6bf939c699091750914a6e962a5c54
zidder <zidder@hilearn.io>
2021-12-10 13:42:26 +0400

    auto refresh flask templates

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 
 @app.route("/<video_id>")
 def hello_world(video_id):
